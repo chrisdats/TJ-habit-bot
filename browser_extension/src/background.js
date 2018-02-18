8c1972831c2647c036cfaffa5bf8e1397ed08c4e
Keller Jordan <kellerjordan1998@gmail.com>
2018-02-17 22:18:51 -0800

    clean up code, remove uneeded files

--- browser_extension/src/background.js
+++ browser_extension/src/background.js
@@ -15,21 +15,26 @@
  */
 
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBhPK2tRDJvXbMqNWWy-qvYx0WPnqMfFsM",
+  authDomain: "monitor-a80a5.firebaseapp.com",
+  databaseURL: "https://monitor-a80a5.firebaseio.com",
+  projectId: "monitor-a80a5",
+  storageBucket: "",
+  messagingSenderId: "134254052426"
+};
 
-pubnub = new PubNub({
-  publishKey : 'pub-c-9008e2a2-1e7e-40a6-aa4e-a9f6ddffb04d',
-  subscribeKey : 'sub-c-c00d7a36-13bb-11e8-a8e8-9e7f09a8f511',
-  ssl: true
-})
+firebase.initializeApp(config);
+db = firebase.database().ref('activity');
 
-function broadcast(message) {
-  var publishConfig = {
-    channel : "tab_activity",
-    message : message
-  }
-  pubnub.publish(publishConfig, function(status, response) {
-      console.log(status, response);
-  })
+function logSessionEvent(session) {
+  var newEntry = activityRef.push();
+  newEntry.set({
+    url: session.url,
+    startTime: session.startTime,
+    endTime: session.endTime
+  });
 }
 
 function formatTime(t) {
@@ -37,19 +42,46 @@ function formatTime(t) {
   return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
 }
 
-var BAD_HOSTNAMES = ["www.facebook.com", "www.reddit.com"];
-
 function onSessionStart(session) {
-  console.log("START", formatTime(session.startTime), session.url);
-  var url = new URL(session.url);
-  var message = BAD_HOSTNAMES.includes(url.hostname) ? 'BAD' : 'GOOD'
-  broadcast(message);
-  if (true && message == 'BAD') {
-    alert('Alert: You indicated that you would like to avoid this website!');
-  }
+  console.log("OPEN", formatTime(session.startTime), session.url);
 }
 
 function onSessionEnd(session) {
-  console.log("END", formatTime(session.endTime), session.url);
+  console.log("CLOSE", formatTime(session.endTime), session.url);
+  logSessionEvent(session);
 }
+
 var stopTracking = startTrackingActivity(onSessionStart, onSessionEnd);
+
+
+  // console.log("START", formatTime(session.startTime), session.url);
+
+  // var newEntry = db.push();
+  // newEntry.set({
+  //   url: 
+  // });
+
+  // db.set(url.hostname);
+  // db.once('value').then(snapshot => console.log(snapshot.val()));
+
+  // var message = BAD_HOSTNAMES.includes(url.hostname) ? 'BAD' : 'GOOD'
+  // broadcast(message);
+  // if (true && message == 'BAD') {
+  //   alert('Alert: You indicated that you would like to avoid this website!');
+  // }
+
+// pubnub = new PubNub({
+//   publishKey : 'pub-c-9008e2a2-1e7e-40a6-aa4e-a9f6ddffb04d',
+//   subscribeKey : 'sub-c-c00d7a36-13bb-11e8-a8e8-9e7f09a8f511',
+//   ssl: true
+// });
+
+// function broadcast(message) {
+//   var publishConfig = {
+//     channel : "tab_activity",
+//     message : message
+//   };
+//   pubnub.publish(publishConfig, function(status, response) {
+//       console.log(status, response);
+//   });
+// }
